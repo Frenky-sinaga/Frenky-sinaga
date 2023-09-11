a5cfe3a31546334abadba6cfa8b97035598d0774
Frenky-sinaga <143597078+Frenky-sinaga@users.noreply.github.com>
2023-09-11 14:22:01 +0700

    applikasiku.py

--- applikasiku.py
+++ applikasiku.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import gspread
 from streamlit_gsheets import GSheetsConnection
 
 url = "https://docs.google.com/spreadsheets/d/10xSu47HrGItuIkUL-o8lbnfgFpwrq5kDkrot4EtOIX0/edit?usp=sharing"
