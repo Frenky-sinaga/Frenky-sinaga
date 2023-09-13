cf71993f4a187d733e7aaa4d6f2669ddf5f48a12
Frenky-sinaga <143597078+Frenky-sinaga@users.noreply.github.com>
2023-09-13 17:07:47 +0700

    applikasiku.py

--- applikasiku.py
+++ applikasiku.py
@@ -1,45 +1,48 @@
 import streamlit as st
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account
 import pandas as pd
 
+# Define the path to your JSON key file
+keyfile_path = "D:/CLC/2023/IT/Belajar/Streamlit/masterstore-398408-e5a0bae4d629.json"
 
-# Set up the scope and credentials
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("D:/CLC/2023/IT/Belajar/Streamlit/masterstore-398408-e5a0bae4d629.json", scope)
+# Define the desired OAuth2 scopes
+scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
-client = gspread.authorize(creds)
+# Authenticate using the JSON key file and scopes
+credentials = service_account.Credentials.from_service_account_file(
+    keyfile_path, scopes=scopes
+)
 
-# Access your Google Sheets spreadsheet by title or URL
-spreadsheet = client.open("MasterStores")
+# Authorize the client to access Google Sheets
+gc = gspread.Client(auth=credentials)
+gc.session.verify = False  # To suppress SSL certificate verification warnings
 
-# Access a specific worksheet
-worksheet = spreadsheet.worksheet("Sheet1")  # Replace with your sheet name
+# Open the specific Google Sheets spreadsheet
+spreadsheet = gc.open("MasterStores")
 
+# Select a worksheet by name (e.g., "Sheet1")
+worksheet = spreadsheet.worksheet("Sheet1")
+
+# Get all records from the worksheet and convert to a Pandas DataFrame
 data = worksheet.get_all_records()
-
-# Convert data to a Pandas DataFrame for grouping
 df = pd.DataFrame(data)
 
-# Group the data by a specific column (e.g., "Category")
+# Group the data by the "Koneksi" column
 grouped = df.groupby("Koneksi")
 
-selected_category = st.selectbox("Select a Category", grouped.groups.keys())
+# Streamlit UI
+st.title("Google Sheets Data Viewer")
 
-rows_per_page=25
+# User selects a category
+selected_category = st.selectbox("Select a Category", list(grouped.groups.keys()))
 
+# Display the selected category's data
+if selected_category in grouped.groups:
+    selected_data = grouped.get_group(selected_category)
+    st.write(f"Displaying data for {selected_category} category:")
+    st.table(selected_data)
+else:
+    st.warning("Selected category not found.")
 
-# Get the current page from the user's input or default to the first page
-group_data = grouped.get_group(selected_category)
-num_pages = (len(group_data) - 1) // rows_per_page + 1
-page_number = st.number_input("Enter Page Number", min_value=1, max_value=num_pages, step=1, value=1)
-
-
-# Calculate the start and end indices for the current page
-start_index = (page_number - 1) * rows_per_page
-end_index = min(page_number * rows_per_page, len(df))
-# Display the data for the current page in a table
-#st.table(data[start_index:end_index])
-# Display the data for the selected category in a table
-st.write(f"Displaying data for {selected_category} category (Page {page_number}/{num_pages}):")
-st.table(group_data[start_index:end_index])
+# Optional: Add pagination or other features as needed
