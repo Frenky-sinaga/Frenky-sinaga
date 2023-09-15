87216c9d74c75330f141d308e42d9b957c6dd27f
Frenky-sinaga <143597078+Frenky-sinaga@users.noreply.github.com>
2023-09-15 15:06:37 +0700

    Update applikasiku.py

--- applikasiku.py
+++ applikasiku.py
@@ -1,94 +1,28 @@
 import streamlit as st
-import gspread
-from google.oauth2 import service_account
-import pandas as pd
-from matplotlib import pyplot as plt
-from gspread_dataframe import get_as_dataframe
-from pandasql import sqldf
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
 
-def authenticate_google_sheets():
-    credentials = service_account.Credentials.from_service_account_file(
-        "masterstore-398408-5f63e59d0f2c.json", scopes=["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"]
-    )
-    gc = gspread.Client(auth=credentials)
-    gc.login()
-    return gc
+st.write("# Welcome to Streamlit! 👋")
 
-# Authenticate and get the Google Sheets client
-gc = authenticate_google_sheets()
+st.sidebar.success("Select a demo above.")
 
-# Open the specific Google Sheets spreadsheet
-spreadsheet = gc.open("MasterStores")
-
-# Select a worksheet by name (e.g., "Sheet1")
-worksheet = spreadsheet.worksheet("Sheet1")
-
-# Get all records from the worksheet and convert to a Pandas DataFrame
-data = worksheet.get_all_records()
-df = pd.DataFrame(data)
-
-# Group the data by the "Koneksi" column
-grouped = df.groupby("Koneksi")
-
-
-# Streamlit UI
-st.title("Master Koneksi toko Cilacap")
-
-# User selects a category
-
-
-selected_category = st.sidebar.selectbox("Tampilkan toko dengan koneksi", list(grouped.groups.keys()))
-
-# Display the selected category's data
-if selected_category in grouped.groups:
-    selected_data = grouped.get_group(selected_category)
-    st.write(f"Tampilkan toko dengan koneksi {selected_category} category:")
-    st.table(selected_data)
-else:
-    st.warning("Selected category not found.")
-
-
-# Optional: Add pagination or other features as needed
-#---------------------------------------------------------------------------
-
-test = st.sidebar.radio("Navigation", ['Home', 'Columns', 'Tabs', 'expander & container', 'Status Elements'])
-
-
-if test == "Columns":
- #   st.subheader("Hai, Sahabat Kelas Awan Pintar :wave:")
- #  st.write("Kita akan belajar di halaman ini menggunakan fungsi Column")
-
-    col1, col2 = st.columns([3, 1])
-
-    # Define your SQL-like query
-
-    # Fetch data from Google Sheets and convert to a DataFrame
-    df = get_as_dataframe(worksheet)
-    
-    pysql = lambda q: sqldf(q, globals())
-    
-    # Define your SQL-like query
-    query = 'SELECT Koneksi, COUNT(KODE) as "JLH_TK" FROM df GROUP BY Koneksi ORDER BY JLH_TK'
-
-    # Execute the query
-    result_df = pysql(query)
-
-    col1.subheader("Menampilkan data dengan grafik")
-
-    bar_colors = ['lightblue', 'lightgreen', 'lightcoral', 'lightsalmon', 'lightseagreen']
-    fig, ax = plt.subplots()
-    bars = result_df.plot(kind='bar', x='Koneksi', y='JLH_TK', ax=ax, color=bar_colors)
-    
-    # Add labels on top of each bar
-    for bar in bars.patches:
-        ax.annotate(format(bar.get_height(), '.0f'),
-                    (bar.get_x() + bar.get_width() / 2,
-                     bar.get_height()), ha='center', va='center',
-                    size=10, xytext=(0, 5),
-                    textcoords='offset points', weight='bold')
-        
-
-    col1.pyplot(fig)
-
-
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
